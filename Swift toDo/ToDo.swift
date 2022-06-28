9436f60bdadbca7a02562bb934303b92aca8037c
shreyyg <shreya.goel.0910@gmail.com>
2022-06-28 10:37:48 -0500

    Initial Commit

---
+++ Swift toDo/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  Swift toDo
+//
+//  Created by Shreya Goel on 6/27/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+}
---
+++ Swift toDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift toDo
+//
+//  Created by Paras Goel on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
